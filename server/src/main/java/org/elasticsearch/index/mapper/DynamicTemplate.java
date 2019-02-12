3dd8d95a881a74918eba3abb8dd09ef87006110c
Vladislav Nezhutin <exe-dealer@yandex.ru>
2019-02-12 12:07:20 +0600

    name-3 dynamic tmpl placeholder

--- server/src/main/java/org/elasticsearch/index/mapper/DynamicTemplate.java
+++ server/src/main/java/org/elasticsearch/index/mapper/DynamicTemplate.java
@@ -321,16 +321,14 @@ public class DynamicTemplate implements ToXContentObject {
     private Map<String, Object> processMap(Map<String, Object> map, String name, String dynamicType) {
         Map<String, Object> processedMap = new HashMap<>();
         for (Map.Entry<String, Object> entry : map.entrySet()) {
-            String key = entry.getKey().replace("{name}", name).replace("{dynamic_type}", dynamicType)
-                .replace("{dynamicType}", dynamicType);
+            String key = processString(entry.getKey(), name, dynamicType);
             Object value = entry.getValue();
             if (value instanceof Map) {
                 value = processMap((Map<String, Object>) value, name, dynamicType);
             } else if (value instanceof List) {
                 value = processList((List) value, name, dynamicType);
             } else if (value instanceof String) {
-                value = value.toString().replace("{name}", name).replace("{dynamic_type}", dynamicType)
-                    .replace("{dynamicType}", dynamicType);
+                value = processString(value.toString(), name, dynamicType);
             }
             processedMap.put(key, value);
         }
@@ -345,13 +343,21 @@ public class DynamicTemplate implements ToXContentObject {
             } else if (value instanceof List) {
                 value = processList((List) value, name, dynamicType);
             } else if (value instanceof String) {
-                value = value.toString().replace("{name}", name)
-                    .replace("{dynamic_type}", dynamicType)
-                    .replace("{dynamicType}", dynamicType);
+                value = processString(value.toString(), name, dynamicType);
             }
             processedList.add(value);
         }
         return processedList;
+    }
+
+    private String processString(String tmpl, String name, String dynamicType) {
+        if (name.length() >= 3) {
+          tmpl = tmpl.replace("{name-3}", name.substring(0, name.length() - 3));
+        }
+        return tmpl
+            .replace("{name}", name)
+            .replace("{dynamic_type}", dynamicType)
+            .replace("{dynamicType}", dynamicType);
     }
 
     @Override
